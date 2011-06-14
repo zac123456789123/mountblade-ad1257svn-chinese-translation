62bdb581d966d8b0444874ef162d847cd1eabb79
aflyhorse <aflyhorse@42fe1520-83f1-ba86-4c6d-cd6d7112b9ec>
2011-06-14 07:22:48 +0000

    two city changed. batch refined.

--- cns/convert_中英文字体转换.bat
+++ cns/convert_中英文字体转换.bat
@@ -1,23 +1,28 @@
 @echo off
-rem Judge if the font has been deleted
-if not exist ..\..\Textures\font.dds ( call switch_默认字体-手写体转换.bat )
+
 rem Judge the current language version
 if exist ..\..\Textures\font.dds.en ( goto cn2en ) else ( goto en2cn )
 
 :cn2en
+echo Now it is Chinese version, change it? Close the window to exit.
+echo 目前字体为中文字体，确认切换请按任意键，取消请关闭本窗口。
+pause
 echo Ready to convert Chinese fonts back to English
 echo 准备将中文字体替换回英文字体
 echo --------------------------------------
 @echo on
-del ..\..\Data\font_data.xml ..\..\Textures\font.dds
+if exist ..\..\Textures\font.dds ( del ..\..\Data\font_data.xml ..\..\Textures\font.dds )
 rename ..\..\Data\font_data.xml.en font_data.xml
 rename ..\..\Textures\font.dds.en font.dds
 @echo off
 goto eof
 
 :en2cn
-echo Ready to convert English fonts back to Chinese
-echo 准备将英文字体替换为中文字体
+echo Now it is English version, change it? Close the window to exit.
+echo 目前字体为英文字体，确认切换请按任意键，取消请关闭本窗口。
+pause
+echo Ready to convert English fonts back to Chinese (handwriting style)
+echo 准备将英文字体替换为中文手写体
 echo --------------------------------------
 @echo on
 rename ..\..\Data\font_data.xml font_data.xml.en
@@ -30,8 +35,8 @@ goto eof
 :eof
 @echo.
 echo --------------------------------------
-echo Conversion done successfully, Have joy! Enter to continue.
-echo 替换成功，飞马祝您游戏愉快！请点击回车继续。
+echo Conversion done successfully, Have joy! Press any key to continue.
+echo 替换成功，飞马祝您游戏愉快！请点击任意键继续。
 @echo.
 echo 更多游戏详情请访问骑马与砍杀中文站-AD1257专区
 echo http://bbs.mountblade.com.cn/forumdisplay.php?fid=92
--- cns/switch_默认字体-手写体转换.bat
+++ cns/switch_默认字体-手写体转换.bat
@@ -1,12 +1,22 @@
 @echo off
 rem Judge if it is now English version
-if not exist ..\..\Textures\font.dds.en ( call convert_中英文字体转换.bat )
+if not exist ..\..\Textures\font.dds.en ( goto break ) else ( goto continue )
+
+:break
+echo It is English version now! I'll run convert script first.
+echo 目前是英文版本的字库！将先运行convert脚本切换回中文。
+call convert_中英文字体转换.bat
+
+:continue
 rem Judge the current font version
 if exist ..\..\Textures\font.dds ( goto hand2norm ) else ( goto norm2hand )
 
 :hand2norm
+echo Now it is handwriting style, change it? Close the window to exit.
+echo 目前字体为手写体，确认切换请按任意键，取消请关闭本窗口。
+pause
 echo Ready to convert Chinese fonts to normal one
-echo 准备将中文字体转换为宋体
+echo 准备将中文字体转换为楷体
 echo --------------------------------------
 @echo on
 del ..\..\Data\font_data.xml ..\..\Textures\font.dds
@@ -14,6 +24,9 @@ del ..\..\Data\font_data.xml ..\..\Textures\font.dds
 goto eof
 
 :norm2hand
+echo Now it is MB standard style, change it? Close the window to exit.
+echo 目前字体为楷体，确认切换请按任意键，取消请关闭本窗口。
+pause
 echo Ready to convert Chinese fonts back to hand-written one
 echo 准备将中文字体转换回手写体
 echo --------------------------------------
@@ -26,8 +39,8 @@ goto eof
 :eof
 @echo.
 echo --------------------------------------
-echo Convertion done successfully, Have joy! Enter to continue.
-echo 转换成功，飞马祝您游戏愉快！请点击回车继续。
+echo Convertion done successfully, Have joy! Press any key to continue.
+echo 转换成功，飞马祝您游戏愉快！请点击任意键继续。
 @echo.
 echo 更多游戏详情请访问骑马与砍杀中文站-AD1257专区
 echo http://bbs.mountblade.com.cn/forumdisplay.php?fid=92
